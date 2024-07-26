eb83f28726d59ceb535bd08c2a12e1611609592c
ali amir <ali.amirsultan0317@gmail.com>
2024-07-26 19:04:37 +0500

    contact us changing in index

--- assets/javascripts/contactform.js
+++ assets/javascripts/contactform.js
@@ -1,6 +1,6 @@
 $(function () {
     "use strict";
-    
+    emailjs.init("C_AI9jjXDrYnzIXF5");
     /*  
         Template name    : Technoit - IT Solutions & Business Services Multipurpose Responsive Website Template
         Author           : ZRTHEMES
@@ -8,21 +8,40 @@ $(function () {
         File Description : contact js file of the template
     */
     $('#ajax-contact').validator();
-    $('#ajax-contact').on('submit', function (e) {
-        if (!e.isDefaultPrevented()) {
-            var url = "assets/phpscripts/contact.html";
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    $( "#msgSubmit" ).removeClass( "hidden" );
-                    $('#ajax-contact')[0].reset();
-                    
-                }
+    $('#ajax-contact').on('submit', function(e) {
+        e.preventDefault(); // Prevent the default form submission
+        
+        const data = $(this).serialize();
+        console.log(data); // Log the serialized data for debugging
+
+        // Parse the serialized data into an object
+        const formData = data.split('&').reduce((obj, item) => {
+            const [key, value] = item.split('=');
+            obj[decodeURIComponent(key)] = decodeURIComponent(value.replace(/\+/g, ' '));
+            return obj;
+        }, {});
+
+        // Create templateParams object
+        const templateParams = {
+            from_name: formData.name,
+            from_email: formData.email,
+            subject: formData.subject,
+            message: formData.message
+        };
+
+        // Send the email using EmailJS
+        emailjs.send('service_1v4pc7f', 'template_5j8ichq', templateParams)
+            .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+                $("#msgSubmit").removeClass("hidden");
+                setTimeout(() => {
+                    $("#msgSubmit").addClass("hidden");
+                }, 5000);
+                $('#ajax-contact')[0].reset(); // Reset the form
+            }, function(error) {
+                alert('FAILED...', error);
             });
-            return false;
-        }
-    })
+
+        return false; // Prevent default form submission
+    });
 });
